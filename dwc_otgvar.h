5f48b8c1133d3505815af540fee18ad13694b2ed
Nick Hudson <skrll@netbsd.org>
2012-11-27 08:15:24 +0000

    Add copyright Add multiple inclusion protection Re-arrange to match FreeBSD layout Add some members to struct dwc_otg_td, by syncing with latest FreeBSD Add struct dwc_otg_td_temp Delete dwc_otg_device_request - don't think we'll have one.

--- dwc_otgvar.h
+++ dwc_otgvar.h
@@ -1,5 +1,109 @@
 /*	$NetBSD: $ */
 
+/* $FreeBSD: src/sys/dev/usb/controller/dwc_otg.h,v 1.12 2012/09/27 15:23:38 hselasky Exp $ */
+/*-
+ * Copyright (c) 2012 Hans Petter Selasky. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef	_DWC_OTGVAR_H_
+#define	_DWC_OTGVAR_H_
+
+#define	DWC_OTG_MAX_DEVICES MIN(USB_MAX_DEVICES, 32)
+#define	DWC_OTG_FRAME_MASK 0x7FF
+#define	DWC_OTG_MAX_TXP 4
+#define	DWC_OTG_MAX_TXN (0x200 * DWC_OTG_MAX_TXP)
+#define	DWC_OTG_MAX_CHANNELS 16
+#define	DWC_OTG_MAX_ENDPOINTS 16
+#define	DWC_OTG_HOST_TIMER_RATE 10 /* ms */
+
+struct dwc_otg_td;
+
+typedef uint8_t (dwc_otg_cmd_t)(struct dwc_otg_td *td);
+
+typedef struct dwc_otg_td {
+	struct dwc_otg_td *obj_next;
+	dwc_otg_cmd_t *func;
+	usbd_xfer_handle xfer;
+ 	void *buf;
+//  	usb_dma_t *dmap;
+	uint32_t tx_bytes;
+	uint32_t offset;
+	uint32_t remainder;
+	uint32_t hcchar;		/* HOST CFG */
+	uint32_t hcsplt;		/* HOST CFG */
+	uint16_t max_packet_size;	/* packet_size */
+	uint16_t npkt;
+	uint8_t errcnt;
+	uint8_t tmr_res;
+	uint8_t tmr_val;
+	uint8_t curr_frame;
+	uint8_t	ep_no;
+	uint8_t channel;
+	uint8_t state;
+#define	DWC_CHAN_ST_START 0
+#define	DWC_CHAN_ST_WAIT_ANE 1
+#define	DWC_CHAN_ST_WAIT_S_ANE 2
+#define	DWC_CHAN_ST_WAIT_C_ANE 3
+#define	DWC_CHAN_ST_RX_PKT 4
+#define	DWC_CHAN_ST_RX_SPKT 5
+#define	DWC_CHAN_ST_RX_SPKT_SYNC 6
+#define	DWC_CHAN_ST_TX_PKT 4
+#define	DWC_CHAN_ST_TX_CPKT 5
+#define	DWC_CHAN_ST_TX_PKT_SYNC 6
+	uint8_t	error:1;
+	uint8_t	error_any:1;
+	uint8_t	error_stall:1;
+	uint8_t	alt_next:1;
+	uint8_t	short_pkt:1;
+	uint8_t	did_stall:1;
+	uint8_t toggle:1;
+	uint8_t set_toggle:1;
+	uint8_t got_short:1;
+	uint8_t did_nak:1;
+} dwc_otg_td_t;
+
+
+struct dwc_otg_std_temp {
+	dwc_otg_cmd_t *func;
+	usbd_xfer_handle xfer;
+ 	void *buf;
+//  	usb_dma_t *dmap;
+	struct dwc_otg_td *td;
+	struct dwc_otg_td *td_next;
+	uint32_t len;
+	uint32_t offset;
+	uint16_t max_frame_size;
+	uint8_t	short_pkt;
+
+	/*
+	 * short_pkt = 0: transfer should be short terminated
+	 * short_pkt = 1: transfer should not be short terminated
+	 */
+	uint8_t	setup_alt_next;
+	uint8_t did_stall;
+	uint8_t bulk_or_control;
+};
 
 struct dwc_otg_flags {
 	uint8_t	change_connect:1;
@@ -21,63 +125,6 @@ struct dwc_otg_flags {
 	uint8_t	d_pulled_up:1;
 };
 
-struct dwc_otg_td;
-
-#if 0
-typedef uint8_t (dwc_otg_cmd_t)(struct dwc_otg_td *td);
-#endif
-
-typedef struct dwc_otg_td {
-	struct dwc_otg_td *obj_next;
-#if 0
-	dwc_otg_cmd_t *func;
-	struct usb_page_cache *pc;
-#else
-	usbd_xfer_handle xfer;
-	usb_dma_t *dmap;
-#endif
-
-	uint32_t tx_bytes;
-	uint32_t offset;
-	uint32_t remainder;
-	uint32_t hcchar;		/* HOST CFG */
-	uint32_t hcsplt;		/* HOST CFG */
-	uint16_t max_packet_size;	/* packet_size */
-	uint16_t npkt;
-	uint8_t errcnt;
-	uint8_t tmr_res;
-	uint8_t tmr_val;
-	uint8_t	ep_no;
-	uint8_t channel;
-	uint8_t state;
-#define	DWC_CHAN_ST_START 0
-#define	DWC_CHAN_ST_WAIT_ANE 1
-#define	DWC_CHAN_ST_WAIT_S_ANE 2
-#define	DWC_CHAN_ST_WAIT_C_ANE 3
-#define	DWC_CHAN_ST_RX_PKT 4
-#define	DWC_CHAN_ST_RX_SPKT 5
-#define	DWC_CHAN_ST_TX_PKT 4
-#define	DWC_CHAN_ST_TX_CPKT 5
-	uint8_t	error:1;
-	uint8_t	error_any:1;
-	uint8_t	error_stall:1;
-	uint8_t	alt_next:1;
-	uint8_t	short_pkt:1;
-	uint8_t	did_stall:1;
-	uint8_t toggle:1;
-	uint8_t set_toggle:1;
-	uint8_t got_short:1;
-	uint8_t did_nak:1;
-} dwc_otg_td_t;
-
-
-#define	DWC_OTG_MAX_DEVICES MIN(USB_MAX_DEVICES, 32)
-#define	DWC_OTG_FRAME_MASK 0x7FF
-#define	DWC_OTG_MAX_TXP 4
-#define	DWC_OTG_MAX_TXN (0x200 * DWC_OTG_MAX_TXP)
-#define	DWC_OTG_MAX_CHANNELS 16
-#define	DWC_OTG_MAX_ENDPOINTS 16
-#define	DWC_OTG_HOST_TIMER_RATE 10 /* ms */
 
 typedef struct dwc_otg_soft_ed {
 } dwc_otg_soft_ed_t;
@@ -136,10 +183,12 @@ typedef struct dwc_otg_softc {
 	uint32_t sc_fifo_size;
 	uint32_t sc_irq_mask;
 	uint32_t sc_last_rx_status;
-
+	uint32_t sc_out_ctl[DWC_OTG_MAX_ENDPOINTS];
+	uint32_t sc_in_ctl[DWC_OTG_MAX_ENDPOINTS];
 	struct dwc_otg_chan_state sc_chan_state[DWC_OTG_MAX_CHANNELS];
 	uint32_t sc_hprt_val;
 	uint32_t sc_tmr_val;	/* timer value */
+	uint16_t sc_active_rx_ep;
 
 	uint8_t sc_timer_active;
 	uint8_t	sc_dev_ep_max;
@@ -149,11 +198,10 @@ typedef struct dwc_otg_softc {
 	uint8_t sc_addr;		/* device address */
 	uint8_t sc_conf;		/* device configuration */
 	uint8_t sc_mode;		/* mode of operation */
-#define DWC_MODE_OTG 0
-#define DWC_MODE_DEVICE 1
-#define DWC_MODE_HOST 2
+#define	DWC_MODE_OTG 0		/* both modes */
+#define	DWC_MODE_DEVICE 1	/* device only */
+#define	DWC_MODE_HOST 2		/* host only */
 
-	uint16_t sc_active_rx_ep;
 
 	struct dwc_otg_flags sc_flags;
 
@@ -167,5 +215,5 @@ void		dwc_otg_childdet(device_t, device_t);
 int		dwc_otg_activate(device_t, enum devact);
 bool		dwc_otg_resume(device_t, const pmf_qual_t *);
 bool		dwc_otg_suspend(device_t, const pmf_qual_t *);
-usbd_status dwc_otg_device_request(usbd_xfer_handle);
 
+#endif	/* _DWC_OTGVAR_H_ */
